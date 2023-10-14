c8df85789ebc9f9f5e245766709437fda02270e7
ByK95 <byrmkaya97@gmail.com>
2023-10-14 13:08:57 +0300

    fix logging

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import shutil
 import logging
 
 app = Flask(__name__)
-app.logger.setLevel(logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 UPLOAD_FOLDER = 'uploads'
 OUTPUT_FOLDER = 'outputs'
@@ -27,7 +27,7 @@ def clean_upload_folder(folder_path, zip_path):
 def unzip_to_folder(zip_path, extract_to):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         root_names = {name.split('/')[0] for name in zip_ref.namelist()}
-        app.logging.info(f"Unziping: {zip_path}, root_names: {root_names}, extract to: {extract_to}")
+        logging.info(f"Unziping: {zip_path}, root_names: {root_names}, extract to: {extract_to}")
         if len(root_names) > 1:
             extract_to = os.path.join(extract_to, list(root_names)[0])
         
@@ -45,7 +45,7 @@ def upload_file():
         
         folder_path = unzip_to_folder(zip_path, UPLOAD_FOLDER)
         folder = os.path.basename(uploaded_file.filename)
-        app.logging.info(f"Folder {folder}")
+        logging.info(f"Folder {folder}")
         
         anim_bin_path = os.path.join(folder_path, 'anim.bin')
         build_bin_path = os.path.join(folder_path, 'build.bin')
